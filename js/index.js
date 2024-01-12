6e2bca445e197acdd7f5167ed4ffe47d1bb0a775
chunb1 <samtw1024@gmail.com>
2024-01-12 12:44:41 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const plusBtnList = document.querySelectorAll('.plus')
 const minusBtnList = document.querySelectorAll('.minus')
 const answerList = document.querySelectorAll('.answer')
+const hamburgerBtn = document.querySelector(".hamburger-button")
+const optionMenu = document.querySelector(".hamburger-menu")
 
 plusBtnList.forEach(element => {
     element.addEventListener('click', () =>{
@@ -19,3 +21,12 @@ minusBtnList.forEach(element => {
         plusBtnList[index].style.display = 'block'
     })
 });
+
+hamburgerBtn.addEventListener('click', () => {
+    if (optionMenu.style.display == 'none'){
+        optionMenu.style.display = 'flex'
+    }
+    else{
+        optionMenu.style.display = 'none'
+    }
+})
