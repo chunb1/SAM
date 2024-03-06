6aef8c74d80b8c0ac9e682acb5766fcab3e4e6c0
chunb1 <samtw1024@gmail.com>
2024-03-06 12:44:58 -0500

    commit

---
+++ js/strawberries.js
@@ -0,0 +1,30 @@
+// const featuresBtn = document.querySelector(".features")
+// const featureMenu = document.querySelector(".feature-options")
+// const companyBtn = document.querySelector(".company")
+// const companyMenu = document.querySelector(".")
+
+// hamburgerBtn.addEventListener('click', () => {
+//     if (optionMenu.style.display == 'none'){
+//         optionMenu.style.display = 'flex'
+//     }
+//     else{
+//         optionMenu.style.display = 'none'
+//     }
+// })
+
+// featuresBtn.addEventListener('click', () => {
+//     if (featureMenu.style.display == 'none'){
+//         featureMenu.style.display = 'flex'
+//     }
+//     else{
+//         featureMenu.style.display = 'none'
+//     }
+// })
+
+const hamburgerBtn = document.querySelector(".hamburger-button")
+
+hamburgerBtn.addEventListener('click', () => {
+    
+})
+
+
